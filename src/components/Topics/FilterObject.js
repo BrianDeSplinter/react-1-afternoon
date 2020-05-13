9d6048dfbe3993ecc98df440592275cb25dd59ea
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-12 22:54:58 -0600

    Solutions 1-4

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react'
+
+class FilterObject extends Component{
+    constructor(){
+        super()
+        this.state = {
+            unFilteredArray: [
+                {
+                    name: 'Brian',
+                    title: 'Boss',
+                    age: 28
+                },
+                {
+                    name: 'Brittney',
+                    relation: 'Wife',
+                    haircolor: 'red'
+                },
+                {
+                    name: 'Evan',
+                    relation: 'Brother',
+                    isDumb: true
+                }],
+            userInput: '',
+            filteredArray: []
+        }
+    }
+
+    render(){
+        return(
+            <div className= 'puzzleBox filterObjectPB'>
+                <h4>Filter Object</h4>
+                <span className= 'puzzleText'>Original: {JSON.stringify(this.state.unFilteredArray, null, 10)}</span>
+                <input className='inputLine' onChange= {(e) => this.handleChange(e.target.value)} />
+                <button className= 'confirmationButton' onClick= {() => this.filterArray(this.state.userInput)} >Filter</button>
+                <span className= 'resultsBox filterObjectRB'> Filtered: {JSON.stringify(this.state.filteredArray, null, 10)} </span>
+            </div>
+        )
+    }
+
+    handleChange(val){
+        this.setState({userInput: val})
+    }
+    
+    filterArray(input){
+        let arr = this.state.unFilteredArray
+        let filterArr = []
+
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i].hasOwnProperty(input)){
+                filterArr.push(arr[i])
+            }
+        }
+
+        this.setState({filteredArray: filterArr})
+
+    }
+}
+
+export default FilterObject
---
+++ src/components/Topics/FilterString.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+class FilterString extends Component{
+    
+    constructor(){
+        super()
+        this.state = {
+            unFilteredArray: ['Brian', 'Brittney', 'Evan', 'Kristin', 'Toni', 'Emma', 'Robert', 'Julie'],
+            userInput: '',
+            filteredArray: ''
+        }
+    }
+    render(){
+        return(
+            <div className= 'puzzleBox filterStringPB'>
+                <h4>Filter String</h4>
+                <span className= 'puzzleText'></span>
+                <input className= 'inputLine' />
+                <button className= 'confirmationButton' ></button>
+                <span className= 'resultsBox filterStringRB' ></span>
+            </div>
+        )
+    }
+}
+
+export default FilterString
