8cadc9d55c909c26e2a293aa32f7c8f0c9bfa65e
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-12 16:05:45 -0600

    Solution for Sum

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,43 @@
 import React, {Component} from 'react'
 
 class Sum extends Component{
+    constructor(){
+        super()
+        this.state={
+            number1: 0,
+            number2: 0,
+            sum: null
+        }
+    }
+    
+    handleFirst(e){
+        this.setState({number1: Number(e)})
+    }
+
+    handleSecond(e){
+        this.setState({number2: Number(e)})
+    }
+
+    sum(){
+        let sum = this.state.number1 + this.state.number2
+        this.setState({sum: sum})
+    }
+
     render(){
         return(
-            <p>Sum Component</p>
+            <div className='puzzleBox sumPB'>
+                <h4>Sum</h4>
+                <input 
+                    className='inputLine'
+                    onChange={(e) => this.handleFirst(e.target.value)}></input>
+                <input 
+                    className='inputLine'
+                    onChange={(e) => this.handleSecond(e.target.value)}></input>
+                <button 
+                    className='confirmationButton'
+                    onClick={(e) => this.sum()}>Add</button>
+                <span className='resultsBox'>The sum is {this.state.sum}</span>
+            </div>
         )
     }
 }
